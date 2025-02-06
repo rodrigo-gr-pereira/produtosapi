6bf9141293ccc3cc958bc70a4cff3ff46f8e8b17
Rodrigo Pereira <rodrigogr.pereira@gmail.com>
2025-02-06 18:02:32 -0300

    inicial

---
+++ src/test/java/io/github/springbootexpert/produtosapi/ProdutosApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.springbootexpert.produtosapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProdutosApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
